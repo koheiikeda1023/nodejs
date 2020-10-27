a033af00d54c3b3c21787cd66628dfa533ab4ed2
koheiikeda1023 <i.kohei2323@gmail.com>
2020-10-27 17:55:27 +0900

    やり直し２

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var http = require('http');
+// var express = require('express');
+
+var port = process.env.PORT || 8000;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+  res.end('Hello World');
+}).listen(process.env.PORT || 8000, '127.0.0.1')
+    console.log(`Server running.at http://127.0.0.1:8000/`);
